eac325bc9084ad3f4eeef965aac10914ce8d360a
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-12-09 16:42:45 +0100

    init repo, install package.json

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let total = 100;
+// let name: string = "Alex";
+let isActive = false;
+// name = "5";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let total: number = 100;
+// let name: string = "Alex";
+let isActive: boolean = false;
+
+// name = "5";

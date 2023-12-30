0ec46a7c2bf9f1a3125408a52ba24c8e6a19097a
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-12-30 14:15:18 +0100

    init

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ let total = 100;
 // let name: string = "Alex";
 let isActive = false;
 // name = "5";
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+console.log(numbers);
+const user = {
+    name: "Alex",
+    age: 18,
+};
+const data = {
+    name: "John Doe",
+    phoneNumber: 1234567890,
+    email: "john@doe.com",
+};
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,23 @@ let total: number = 100;
 let isActive: boolean = false;
 
 // name = "5";
+const numbers: number[] = [1, 2, 3, 4, 5];
+numbers.push(6);
+console.log(numbers);
+
+const user: { name: string; age: number } = {
+  name: "Alex",
+  age: 18,
+};
+
+type userData = {
+  name: string;
+  phoneNumber: number;
+  email: string;
+};
+
+const data: userData = {
+  name: "John Doe",
+  phoneNumber: 1234567890,
+  email: "john@doe.com",
+};
